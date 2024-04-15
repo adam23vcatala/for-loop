3a13089cd0ff3b3a2a0358dadef6b5f422b4d2e5
Víctor Català Mendoza <adam23vcatala@inslaferreria.cat>
2024-04-15 19:43:38 +0200

    Calculadora creada

---
+++ app/src/app/main.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        String texto = "No debería abusar de nadie por su aspecto";
+        System.out.println("Introduce un número: ");
+        if (!sc.hasNextInt()) {
+            System.err.println("Tiene que ser un número entero");
+        } else if (num < 0){
+            System.err.println("Tiene que ser positivo");
+        }
+        num = sc.nextInt();
+        
+        for (int i = 1; i <= num; i++) {
+            System.out.println(i + ". " + texto);
+        }
+    }
+    
+}
